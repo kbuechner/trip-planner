411cfe96e656e8080d730f219836e4b3424ef8bd
Katherine Buechner <kbuechner@gmail.com>
2016-05-13 11:14:27 -0400

    Day adding button complete, adding items complete, need to implement removal of items & days

--- public/js/main.js
+++ public/js/main.js
@@ -8,6 +8,7 @@
 //get it so when we click, we are added to the day
 var currDay = 1;
 var numOfDays = 1;
+var itinerary = {};
 
 function addHotels(){
 	hotels.forEach(function(hotel){
@@ -15,17 +16,6 @@ function addHotels(){
 	})
 
 }
-
-
-
-$(".addDay").on("click", function(){
-	
-	numOfDays++;
-
-
-	$(".badge-day-display").append("<p class='badge day' id = " + numOfDays +">" + numOfDays+ "</p>")
-	console.log($("#"+numOfDays));
-});
 
 function addRestaurant(){
 	restaurants.forEach(function(restaurant){
@@ -40,18 +30,41 @@ function addActivities(){
 
 }
 
+//addToDay (string, string) => updates dom
+function addToDay(item, table){
+	var dayEl = $("#"+table+"-list")
+	dayEl.append("<li>"+item+"</li><span class='glyphicon glyphicon-remove-circle' aria-hidden='true'></span></br>");
+	console.log("HERE", dayEl);
+}
+
+//selects item like hotel 
 $("select").change(function(e){
 	var item = this.value;
 	var table = $(this).attr("data-type");
-
+	console.log(item, table, currDay);
+	addToDay(item, table);
 });
-$(".day").on("click", function(e){
+
+
+ //  ____    _ __   ______  
+ // |  _ \  / \\ \ / / ___| 
+ // | | | |/ _ \\ V /\___ \ 
+ // | |_| / ___ \| |  ___) |
+ // |____/_/   \_\_| |____/ 
+                         
+//set current day
+$(".badge-day-display").on("click", ".day", function(e){
 	console.log(this);
-	currentDay = e.target.id;
-	console.log(e.target.id)	
+	currDay = e.target.id;
+	$(".numDay").html("Day "+currDay) 
+	console.log("currDay", currDay)	
 })
-
-
+//add a day
+$(".addDay").on("click", function(){
+	numOfDays++;
+	$(".badge-day-display").append("<p class='badge day' id = " + numOfDays +">" + numOfDays+ "</p>")
+	console.log($("#"+numOfDays));
+});
 
 
 addHotels();
